182da0f213db7a6a63185e135469f3670c0e8bed
LudvikWoo <LudvikWoo@users.noreply.github.com>
2018-04-24 08:29:37 +0800

    新增gy_user的mapper文件

---
+++ src/main/java/com/guoyasoft/bean/db/GyUser.java
@@ -0,0 +1,95 @@
+package com.guoyasoft.bean.db;
+
+import java.util.Date;
+
+public class GyUser {
+    private String uno;
+
+    private Integer id;
+
+    private String realName;
+
+    private String userName;
+
+    private String pwd;
+
+    private Integer status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String roleId;
+
+    public String getUno() {
+        return uno;
+    }
+
+    public void setUno(String uno) {
+        this.uno = uno == null ? null : uno.trim();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName == null ? null : realName.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd == null ? null : pwd.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId == null ? null : roleId.trim();
+    }
+}
